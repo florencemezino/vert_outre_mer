902ea0652dd5ac48351819d5c39548b5d67cc83f
Florence Mezino <florence.mezino@outlook.com>
2021-08-17 12:12:59 +0000

    Update link in api restrictions for maps.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,4 +1,4 @@
-/* function initMap() {
+ function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
@@ -26,16 +26,3 @@
         "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
     });
 }
-
-*/
-
-
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
