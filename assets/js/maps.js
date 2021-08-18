ddf7e0014cc0a43ec05eb193ab8a77927c259fd0
Florence Mezino <florence.mezino@outlook.com>
2021-08-18 08:44:39 +0000

    Fix rendering map api

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,16 +1,51 @@
+ 
+ /* map style */
+
+ window.onload = function () {
+  var styles = [
+    {
+        featureType: 'poi',
+        elementType: 'geometry',
+        stylers: [
+          { hue: '#f9e0b7' },
+          { lightness: 30 }
+        ]
+      },{
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [
+          { hue: '#d5c18c' },
+          { lightness: 14 }
+        ]
+      },{
+        featureType: 'road.local',
+        elementType: 'all',
+        stylers: [
+          { hue: '#ffd7a6' },
+          { saturation: 100 },
+          { lightness: -12 }
+        ]
+      }
+  ];
+};
+
+
+
+/* experiences clusters */
+ 
  function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 10,
         center: {
-            lat : 46.619261,
-            lng: -33.134766,
+            lat : 46.7111,
+            lng: 1.7191,
         }
     });
-    var labels = "Event 1";
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var locations = [
-        {lat: 40.785091, lng: -73.968285},
-        {lat: 41.0840045, lng: -73.874245},
-        {lat: 40.754932, lng: -73.984016},
+        {lat: -20.878901, lng: 55.448101},
+        {lat: -21.341900, lng: 55.477798}, 
 
     ];
 
@@ -21,8 +56,16 @@
         });
     });
 
-    new MarkerClusterer(map, markers, {
-        imagePath:
-        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-    });
+    var markerCluster = new MarkerClusterer(map, markers, 
+        {imagePath:"https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"});
 }
+
+/*  Reunion Island 
+{lat: -20.878901, lng: 55.448101}, (Saint Denis)
+{lat: -21.341900, lng: 55.477798}, (Saint Pierre)
+
+French Guiana 
+{lat: 4.937200, lng: -52.326000}, (Cayenne)
+{lat: 5.159700, lng: -52.650299}, (Kourou)
+
+*/
