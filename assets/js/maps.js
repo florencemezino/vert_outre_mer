7624f881f2a4e008c7d5ff796fdd4e7d53328434
Florence Mezino <florence.mezino@outlook.com>
2021-08-18 10:31:53 +0000

    Add all marker cluster experiences on map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,5 +1,5 @@
  
- /* map style */
+ /* map style 
 
  window.onload = function () {
   var styles = [
@@ -29,25 +29,58 @@
   ];
 };
 
+*/
 
 
 /* experiences clusters */
  
  function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 10,
+        zoom: 3,
         center: {
             lat : 46.7111,
             lng: 1.7191,
         }
     });
 
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var labels = "";
     var locations = [
-        {lat: -20.878901, lng: 55.448101},
-        {lat: -21.341900, lng: 55.477798}, 
+        /* Guadeloupe */
+        {lat: 15.99854, lng: -61.72548}, /* Basse Terre, */
+        {lat: 16.20685 , lng: -61.49329}, /* Le Gosier, */
+
+        /* Saint Martin */
+        {lat: 18.073099, lng: -63.082199}, /* Marigot, */
+
+        /* Saint Barthelemy */
+        {lat: 17.900000, lng: -62.8333}, /* Gustavia, */
+
+        /* Martinique */
+        {lat: 14.600000, lng: -61.083302}, /* Fort de France, */
+ 
+        /* French Guiana */
+        {lat: 4.937200, lng: -52.326000}, /*  Cayenne, */
+        {lat: 5.159700, lng: -52.650299}, /*  Kourou, */
+
+        /* Reunion Island */
+        {lat: -20.878901, lng: 55.448101}, /*  Saint Denis, Maison de l'écologie */
+        {lat: -21.341900, lng: 55.477798}, /*  Saint Pierre, Sakifo */
+        {lat: -21.1667, lng: 55.5833}, /*  Plaine des cafres, Cite du Volcan */
+
+        /* Saint Pierre et Miquelon */
+        {lat: 47.0975 , lng: -56.3814}, /* Saint Pierre, */
+
+        /* Mayotte */
+        {lat: -12.7794, lng: 45.2272}, /* Mamoudzou, */
+
+        /* New Caledonia */
+        {lat: -22.275801, lng: 166.457993}, /* Noumea, */
+
+        /* French Polynesia */
+        {lat: -17.535000, lng: -149.569600}, /* Papeete, */
 
     ];
+
 
     var markers = locations.map(function(location, i) {
         return new google.maps.Marker({
@@ -59,13 +92,3 @@
     var markerCluster = new MarkerClusterer(map, markers, 
         {imagePath:"https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"});
 }
-
-/*  Reunion Island 
-{lat: -20.878901, lng: 55.448101}, (Saint Denis)
-{lat: -21.341900, lng: 55.477798}, (Saint Pierre)
-
-French Guiana 
-{lat: 4.937200, lng: -52.326000}, (Cayenne)
-{lat: 5.159700, lng: -52.650299}, (Kourou)
-
-*/
