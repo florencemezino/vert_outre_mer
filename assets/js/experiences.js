2d2afc2c0fbd6183116f5560e6a39c327ca08621
Florence Mezino <florence.mezino@outlook.com>
2021-08-19 10:06:24 +0000

    add event details for 2 destinations

--- assets/js/experiences.js
+++ assets/js/experiences.js
@@ -3,14 +3,25 @@ const destinations = {
         events: [{          
             name : "Sustainable Cities Conference",
 			description : "Learn how Guadeloupe is starting the green revolution to prepare the world of tomorrow",
+			price : "free",
             date : "Sep 22, 2021",
 			localTime: "10.00 am",
             city: "Basse Terre",
             location :[15.99854, -61.72548]
         },
+
+		{          
+            name : "Carribean Festival",
+			description : "Dicover the music of the Carribeans. This is a plastic fre concert. Come have fun with us! ",
+			price : "15 EUR per person",
+            date : "Oct 15, 2021",
+			localTime: "06.00 pm",
+            city: "Le Gosier",
+            location :[16.20685, -61.49329]
+        },
     ],
         description: {
-            title : "Welcome to Guadeloupe",
+			title : "Welcome to Guadeloupe",
             description : "Guadeloupe is an archipelago and overseas department and region of France in the Caribbean. Whether walking through lively village streets, touring country lanes or gliding between island by ferry, Guadeloupe serves up a rich menu of unique cultural experiences.",
         },
     
@@ -18,17 +29,18 @@ const destinations = {
 
 	"Saint Martin" : {
         events: [{          
-            name : "Sustainable Cities Conference",
-			description : "Learn how Guadeloupe is starting the green revolution to prepare the world of tomorrow",
-            date : "Sep 22, 2021",
-			localTime: "02.00 pm",
-            city: "Basse Terre",
-            location :[15.99854, -61.72548]
+            name : "Marigot's beach clean up",
+			description : "Join us this Sunday to clean up Marigot's beach. ",
+			price : "free",
+            date : "Aug 22, 2021",
+			localTime: "04.00 pm",
+            city: "Marigot",
+            location :[18.073099, -63.082199]
         },
     ],
         description: {
-            title : "Welcome to Guadeloupe",
-            description : "Guadeloupe is an archipelago and overseas department and region of France in the Caribbean. Whether walking through lively village streets, touring country lanes or gliding between island by ferry, Guadeloupe serves up a rich menu of unique cultural experiences.",
+			title : "Welcome to Saint Martin",
+            description : "The culture of Saint Martin is a blend of its African, French, British, and Dutch heritage.Saint Martin is known to be the capital of duty free and gastronomy in the Caribbean. The warm hospitality of its people, its diversity and its varied cultural activities all allowed it to be nicknamed the 'Friendly Island'",
 
         },
     
@@ -45,8 +57,8 @@ const destinations = {
         },
     ],
         description: {
-            title : "Welcome to Guadeloupe",
-            description : "Guadeloupe is an archipelago and overseas department and region of France in the Caribbean. Whether walking through lively village streets, touring country lanes or gliding between island by ferry, Guadeloupe serves up a rich menu of unique cultural experiences.",
+            title : "Welcome to Saint Barthelemy",
+            description : "Saint Barthelemy, also called saint Barth is a small tourism-oriented island in the Caribbean with remarkable natural scenery.",
 
         },
     
@@ -63,8 +75,8 @@ const destinations = {
         },
     ],
         description: {
-            title : "Welcome to Guadeloupe",
-            description : "Guadeloupe is an archipelago and overseas department and region of France in the Caribbean. Whether walking through lively village streets, touring country lanes or gliding between island by ferry, Guadeloupe serves up a rich menu of unique cultural experiences.",
+            title : "Welcome to Martinique",
+            description : "Martinique boasts a whole world of natural wonders. The beautiful island offers a seductive mix of magnificent beaches, dramatic mountains, tropical gardens, and fascinating history.",
 
         },
     
@@ -81,8 +93,8 @@ const destinations = {
         },
     ],
         description: {
-            title : "Welcome to Guadeloupe",
-            description : "Guadeloupe is an archipelago and overseas department and region of France in the Caribbean. Whether walking through lively village streets, touring country lanes or gliding between island by ferry, Guadeloupe serves up a rich menu of unique cultural experiences.",
+            title : "Welcome to French Guiana",
+            description : "Located between the Republic of Suriname and Brazil, French Guiana is a kingdom of forests, rivers and animals and seduces all nature lovers",
 
         },
     
@@ -100,13 +112,12 @@ const destinations = {
         },
     ],
         description: {
-            title : "Welcome to Guadeloupe",
-            description : "Guadeloupe is an archipelago and overseas department and region of France in the Caribbean. Whether walking through lively village streets, touring country lanes or gliding between island by ferry, Guadeloupe serves up a rich menu of unique cultural experiences.",
+            title : "Welcome to Reunion Island",
+            description : "Located in the Archipelago of Mascareignes, Reunion Island offers an infinite number of amazing sites and contrasting landscapes. The island is known for the spectacular Piton de la Fournaise, an active basaltic volcano.",
 
         },
     
     },
-
 
 	"Saint Pierre et Miquelon" : {
         events: [{          
@@ -119,8 +130,8 @@ const destinations = {
         },
     ],
         description: {
-            title : "Welcome to Guadeloupe",
-            description : "Guadeloupe is an archipelago and overseas department and region of France in the Caribbean. Whether walking through lively village streets, touring country lanes or gliding between island by ferry, Guadeloupe serves up a rich menu of unique cultural experiences.",
+            title : "Welcome to Saint Pierre et Miquelon",
+            description : "Between the friendly bonjours and the delicate pastries come and discover these two beautiful islands, a real piece of France in North America",
 
         },
     
@@ -137,8 +148,8 @@ const destinations = {
         },
     ],
         description: {
-            title : "Welcome to Guadeloupe",
-            description : "Guadeloupe is an archipelago and overseas department and region of France in the Caribbean. Whether walking through lively village streets, touring country lanes or gliding between island by ferry, Guadeloupe serves up a rich menu of unique cultural experiences.",
+            title : "Welcome to Mayotte",
+            description : "The island of Mayotte has an abundance of endemic fauna and flora. It is an authentic island to visit in the Indian Ocean.",
 
         },
     
@@ -155,8 +166,8 @@ const destinations = {
         },
     ],
         description: {
-            title : "Welcome to Guadeloupe",
-            description : "Guadeloupe is an archipelago and overseas department and region of France in the Caribbean. Whether walking through lively village streets, touring country lanes or gliding between island by ferry, Guadeloupe serves up a rich menu of unique cultural experiences.",
+            title : "Welcome to New Caledonia",
+            description : "Located in the world’s largest lagoon and possessing the second-largest barrier reef, New Caledonia has so much to offer.",
 
         },
     
@@ -173,8 +184,8 @@ const destinations = {
         },
     ],
         description: {
-            title : "Welcome to Guadeloupe",
-            description : "Guadeloupe is an archipelago and overseas department and region of France in the Caribbean. Whether walking through lively village streets, touring country lanes or gliding between island by ferry, Guadeloupe serves up a rich menu of unique cultural experiences.",
+            title : "Welcome to French Polynesia",
+            description : "Exotic wildlife, impeccable cuisine, welcoming locals, French Polynesia is your new tropical vacation destination.",
 
         },
     
