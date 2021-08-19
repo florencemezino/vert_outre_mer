dd4a8b6e1c67e127629c0ecc03354a9d7a0e0f31
Florence Mezino <florence.mezino@outlook.com>
2021-08-19 09:42:12 +0000

    Add every destination to the map

--- assets/js/experiences.js
+++ assets/js/experiences.js
@@ -2,19 +2,184 @@ const destinations = {
     "Guadeloupe" : {
         events: [{          
             name : "Sustainable Cities Conference",
+			description : "Learn how Guadeloupe is starting the green revolution to prepare the world of tomorrow",
             date : "Sep 22, 2021",
+			localTime: "10.00 am",
             city: "Basse Terre",
             location :[15.99854, -61.72548]
         },
     ],
-
         description: {
             title : "Welcome to Guadeloupe",
             description : "Guadeloupe is an archipelago and overseas department and region of France in the Caribbean. Whether walking through lively village streets, touring country lanes or gliding between island by ferry, Guadeloupe serves up a rich menu of unique cultural experiences.",
-            localTime : "2021-01-31T13:00:00",
         },
     
     },
+
+	"Saint Martin" : {
+        events: [{          
+            name : "Sustainable Cities Conference",
+			description : "Learn how Guadeloupe is starting the green revolution to prepare the world of tomorrow",
+            date : "Sep 22, 2021",
+			localTime: "02.00 pm",
+            city: "Basse Terre",
+            location :[15.99854, -61.72548]
+        },
+    ],
+        description: {
+            title : "Welcome to Guadeloupe",
+            description : "Guadeloupe is an archipelago and overseas department and region of France in the Caribbean. Whether walking through lively village streets, touring country lanes or gliding between island by ferry, Guadeloupe serves up a rich menu of unique cultural experiences.",
+
+        },
+    
+    },
+
+	"Saint Barthelemy" : {
+        events: [{          
+            name : "Sustainable Cities Conference",
+			description : "Learn how Guadeloupe is starting the green revolution to prepare the world of tomorrow",
+            date : "Sep 22, 2021",
+			localTime: "02.00 pm",
+            city: "Basse Terre",
+            location :[15.99854, -61.72548]
+        },
+    ],
+        description: {
+            title : "Welcome to Guadeloupe",
+            description : "Guadeloupe is an archipelago and overseas department and region of France in the Caribbean. Whether walking through lively village streets, touring country lanes or gliding between island by ferry, Guadeloupe serves up a rich menu of unique cultural experiences.",
+
+        },
+    
+    },
+
+	"Martinique" : {
+        events: [{          
+            name : "Sustainable Cities Conference",
+			description : "Learn how Guadeloupe is starting the green revolution to prepare the world of tomorrow",
+            date : "Sep 22, 2021",
+			localTime: "02.00 pm",
+            city: "Basse Terre",
+            location :[15.99854, -61.72548]
+        },
+    ],
+        description: {
+            title : "Welcome to Guadeloupe",
+            description : "Guadeloupe is an archipelago and overseas department and region of France in the Caribbean. Whether walking through lively village streets, touring country lanes or gliding between island by ferry, Guadeloupe serves up a rich menu of unique cultural experiences.",
+
+        },
+    
+    },
+
+	"French Guiana" : {
+        events: [{          
+            name : "Sustainable Cities Conference",
+			description : "Learn how Guadeloupe is starting the green revolution to prepare the world of tomorrow",
+            date : "Sep 22, 2021",
+			localTime: "02.00 pm",
+            city: "Basse Terre",
+            location :[15.99854, -61.72548]
+        },
+    ],
+        description: {
+            title : "Welcome to Guadeloupe",
+            description : "Guadeloupe is an archipelago and overseas department and region of France in the Caribbean. Whether walking through lively village streets, touring country lanes or gliding between island by ferry, Guadeloupe serves up a rich menu of unique cultural experiences.",
+
+        },
+    
+    },
+
+
+	"Reunion Island" : {
+        events: [{          
+            name : "Sustainable Cities Conference",
+			description : "Learn how Guadeloupe is starting the green revolution to prepare the world of tomorrow",
+            date : "Sep 22, 2021",
+			localTime: "02.00 pm",
+            city: "Basse Terre",
+            location :[15.99854, -61.72548]
+        },
+    ],
+        description: {
+            title : "Welcome to Guadeloupe",
+            description : "Guadeloupe is an archipelago and overseas department and region of France in the Caribbean. Whether walking through lively village streets, touring country lanes or gliding between island by ferry, Guadeloupe serves up a rich menu of unique cultural experiences.",
+
+        },
+    
+    },
+
+
+	"Saint Pierre et Miquelon" : {
+        events: [{          
+            name : "Sustainable Cities Conference",
+			description : "Learn how Guadeloupe is starting the green revolution to prepare the world of tomorrow",
+            date : "Sep 22, 2021",
+			localTime: "02.00 pm",
+            city: "Basse Terre",
+            location :[15.99854, -61.72548]
+        },
+    ],
+        description: {
+            title : "Welcome to Guadeloupe",
+            description : "Guadeloupe is an archipelago and overseas department and region of France in the Caribbean. Whether walking through lively village streets, touring country lanes or gliding between island by ferry, Guadeloupe serves up a rich menu of unique cultural experiences.",
+
+        },
+    
+    },
+
+	"Mayotte" : {
+        events: [{          
+            name : "Sustainable Cities Conference",
+			description : "Learn how Guadeloupe is starting the green revolution to prepare the world of tomorrow",
+            date : "Sep 22, 2021",
+			localTime: "02.00 pm",
+            city: "Basse Terre",
+            location :[15.99854, -61.72548]
+        },
+    ],
+        description: {
+            title : "Welcome to Guadeloupe",
+            description : "Guadeloupe is an archipelago and overseas department and region of France in the Caribbean. Whether walking through lively village streets, touring country lanes or gliding between island by ferry, Guadeloupe serves up a rich menu of unique cultural experiences.",
+
+        },
+    
+    },
+
+	"New Caledonia" : {
+        events: [{          
+            name : "Sustainable Cities Conference",
+			description : "Learn how Guadeloupe is starting the green revolution to prepare the world of tomorrow",
+            date : "Sep 22, 2021",
+			localTime: "02.00 pm",
+            city: "Basse Terre",
+            location :[15.99854, -61.72548]
+        },
+    ],
+        description: {
+            title : "Welcome to Guadeloupe",
+            description : "Guadeloupe is an archipelago and overseas department and region of France in the Caribbean. Whether walking through lively village streets, touring country lanes or gliding between island by ferry, Guadeloupe serves up a rich menu of unique cultural experiences.",
+
+        },
+    
+    },
+
+	"French Polynesia" : {
+        events: [{          
+            name : "Sustainable Cities Conference",
+			description : "Learn how Guadeloupe is starting the green revolution to prepare the world of tomorrow",
+            date : "Sep 22, 2021",
+			localTime: "02.00 pm",
+            city: "Basse Terre",
+            location :[15.99854, -61.72548]
+        },
+    ],
+        description: {
+            title : "Welcome to Guadeloupe",
+            description : "Guadeloupe is an archipelago and overseas department and region of France in the Caribbean. Whether walking through lively village streets, touring country lanes or gliding between island by ferry, Guadeloupe serves up a rich menu of unique cultural experiences.",
+
+        },
+    
+    },
+
 
 }
 
