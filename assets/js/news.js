371f23117d2aefab875ad18a8336a148114f3bf6
Florence Mezino <florence.mezino@outlook.com>
2021-08-20 14:14:42 +0000

    change photo in contact page

--- assets/js/news.js
+++ assets/js/news.js
@@ -12,3 +12,15 @@ $(document).ready(function() {
         $(this).find('.article-img-holder').css('transform', 'scale(1)');
     });
 });
+
+
+// newsletter close modal
+
+$('#button').submit(function(e) {
+    e.preventDefault();
+    // Coding
+    $('#popup').modal('toggle'); //or  $('#IDModal').modal('hide');
+    return false;
+});
+
+
